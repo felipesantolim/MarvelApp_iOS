dda467d9c4992eb1d7e1b2805dfa31779b03c4f3
Felipe Henrique Santolim <>
2018-09-20 16:18:25 -0300

    first commit

---
+++ MarvelApp_iOS/MarvelApp_iOSTests/MarvelApp_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  MarvelApp_iOSTests.swift
+//  MarvelApp_iOSTests
+//
+//  Created by Felipe Henrique Santolim on 20/09/18.
+//  Copyright © 2018 Felipe Santolim. All rights reserved.
+//
+
+import XCTest
+@testable import MarvelApp_iOS
+
+class MarvelApp_iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ MarvelApp_iOS/Pods/Target Support Files/Pods-MarvelApp_iOSTests/Pods-MarvelApp_iOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MarvelApp_iOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_MarvelApp_iOSTests
+@end
